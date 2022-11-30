77f1f00cd7e46a4f32d494f411aa0f8c065ff2ed
Lights0n <lyadouskiy@gmail.com>
2022-12-01 01:25:25 +0300

    feat: start strings

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -241,8 +241,12 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  // throw new Error('Not implemented');
+  if (value instanceof String || typeof (value) === 'string') {
+    return true;
+  }
+  return false;
 }
 
 
